a41148c1b95f51068e6a001cfd955766b8718c99
3ohn <65126714+3ohn@users.noreply.github.com>
2021-01-23 17:12:07 +0700

    latest

---
+++ gate/6to/script.js
@@ -0,0 +1,533 @@
+// Change the second argument to your options:
+// https://github.com/sampotts/plyr/#options
+const player = new Plyr('video');
+
+// Expose player so it can be used from the console
+window.player = player;
+
+// the question answaer
+const questions = [
+	{
+		'question': [
+            { answer: 'C' },
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'C' },
+            { answer: 'B' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'C' },
+            { answer: 'B' },
+		],
+		'questionText': [
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+		],
+		'video': {
+			'cover' : '',
+			'src': 'videos/g1_u1_to.m4v'
+		}
+	}
+];
+const questionsU2 = [
+	{
+		'question': [
+            { answer: 'C' },
+            { answer: 'C' },
+            { answer: 'C' },
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'B' },
+            { answer: 'B' },
+		],
+		'questionText': [
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+		],
+		'video': {
+			'cover' : '',
+			'src': 'videos/g1_u2_to.m4v'
+		}
+	}
+];
+const questionsU3 = [
+	{
+		'question': [
+            { answer: 'B' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'B' },
+            { answer: 'B' },
+            { answer: 'A' },
+            { answer: 'B' },
+		],
+		'questionText': [
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+		],
+		'video': {
+			'cover' : '',
+			'src': 'videos/g1_u3_to.m4v'
+		}
+	}
+];
+const questionsU4 = [
+	{
+		'question': [
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'C' },
+            { answer: 'C' },
+            { answer: 'B' },
+            { answer: 'B' },
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'C' },
+            { answer: 'C' },
+		],
+		'questionText': [
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+		],
+		'video': {
+			'cover' : '',
+			'src': 'videos/g1_u4_to.m4v'
+		}
+	}
+];
+
+const questionsU5 = [
+	{
+		'question': [
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'C' },
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'B' },
+            { answer: 'A' },
+            { answer: 'C' },
+            { answer: 'A' },
+		],
+		'questionText': [
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+		],
+		'video': {
+			'cover' : '',
+			'src': 'videos/g1_u5_to.m4v'
+		}
+	}
+];
+const questionsU6 = [
+	{
+		'question': [
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'C' },
+            { answer: 'C' },
+            { answer: 'C' },
+            { answer: 'B' },
+            { answer: 'B' },
+		],
+		'questionText': [
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+		],
+		'video': {
+			'cover' : '',
+			'src': 'videos/g1_u6_to.m4v'
+		}
+	}
+];
+const questionsU7 = [
+	{
+		'question': [
+            { answer: 'B' },
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'A' },
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'C' },
+		],
+		'questionText': [
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+		],
+		'video': {
+			'cover' : '',
+			'src': 'videos/g1_u7_to.m4v'
+		}
+	}
+];
+const questionsU8 = [
+	{
+		'question': [
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'A' },
+            { answer: 'A' },
+            { answer: 'A' },
+            { answer: 'A' },
+            { answer: 'A' },
+		],
+		'questionText': [
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+		],
+		'video': {
+			'cover' : '',
+			'src': 'videos/g1_u8_to.m4v'
+		}
+	}
+];
+const questionsU9 = [
+	{
+		'question': [
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'C' },
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'A' },
+            { answer: 'A' },
+            { answer: 'A' },
+            { answer: 'A' },
+		],
+		'questionText': [
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+		],
+		'video': {
+			'cover' : '',
+			'src': 'videos/g1_u9_to.m4v'
+		}
+	}
+];
+const questionsU10 = [
+	{
+		'question': [
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'B' },
+            { answer: 'C' },
+		],
+		'questionText': [
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+		],
+		'video': {
+			'cover' : '',
+			'src': 'videos/g1_u10_to.m4v'
+		}
+	}
+];
+const questionsU11 = [
+	{
+		'question': [
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'C' },
+		],
+		'questionText': [
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+		],
+		'video': {
+			'cover' : '',
+			'src': 'videos/g1_u11_to.m4v'
+		}
+	}
+];
+const questionsU12 = [
+	{
+		'question': [
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'B' },
+            { answer: 'C' },
+            { answer: 'B' },
+            { answer: 'A' },
+            { answer: 'C' },
+            { answer: 'A' },
+            { answer: 'C' },
+		],
+		'questionText': [
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+			{optionA: "A", optionB: "B", optionC:'C'},
+		],
+		'video': {
+			'cover' : '',
+			'src': 'videos/g1_u12_to.m4v'
+		}
+	}
+];
+
+function getQuestionFromUnit(){
+	// var level = $("#testUnit").val();
+	var level = sessionStorage.getItem('unitIframe');
+    var myQuestions = questions;
+    if(level == 1)
+      myQuestions = questions;  
+    if(level == 2)
+      myQuestions = questionsU2;
+    if(level == 3)
+      myQuestions = questionsU3;
+    if(level == 4)
+      myQuestions = questionsU4;
+    if(level == 5)
+      myQuestions = questionsU5;
+    if(level == 6)
+      myQuestions = questionsU6;
+    if(level == 7)
+      myQuestions = questionsU7;
+    if(level == 8)
+      myQuestions = questionsU8;
+    if(level == 9)
+      myQuestions = questionsU9;
+    if(level == 10)
+      myQuestions = questionsU10;
+    if(level == 11)
+      myQuestions = questionsU11;
+    if(level == 12)
+	  myQuestions = questionsU12;
+    return myQuestions
+}
+
+function setStyleOptionAnswer(el, color) {
+	color = typeof color === "undefined" ? "black" : color;
+	el.style.color = color;
+}
+
+function checkAnswer() {
+    var myQuestion = getQuestionFromUnit()[0].question;
+	for(const [i, v] of myQuestion.entries()) {
+		var divWrapper = document.querySelector(`input[name="option_answer_${i+1}"]`).closest('.choices');
+		var optionAnswer = document.querySelector(`input[name="option_answer_${i+1}"]:checked`);
+		
+		setStyleOptionAnswer(divWrapper, "red");
+		if(optionAnswer) {
+			if(optionAnswer.checked) {
+				if(optionAnswer.value == v.answer) setStyleOptionAnswer(divWrapper, "green");
+			}	
+		}
+	}
+}
+
+function getAnswer() {
+    var myQuestion = getQuestionFromUnit()[0].question;
+	for(const [i, v] of myQuestion.entries()) {
+		let optionAnswer = document.querySelectorAll(`input[name="option_answer_${i+1}"]`);
+		for(opt of optionAnswer) {
+			let divWrapper = opt.closest('.choices');
+			setStyleOptionAnswer(divWrapper);
+			if(opt.value == v.answer) opt.checked = true;
+		}
+	}
+} 
+
+function generateQuestionAnswer() {
+    let divQuestionAnswer = document.querySelector('#div-question_answer');
+    var myQuestion = getQuestionFromUnit()[0];
+    for(const [i, v] of myQuestion.question.entries()) {
+        divQuestionAnswer.innerHTML = divQuestionAnswer.innerHTML
+                                    + `<div class="choices">
+                                        <div>
+                                        <p>${(i + 1 < 10) ? "&nbsp;&nbsp;" + (i + 1) : i + 1}</p>
+                                        </div>
+                                        <div>
+                                            <label class="container">${myQuestion.questionText[i].optionA}
+                                                <input type="radio" name="option_answer_${i+1}" class="option-answer" value="A">
+                                                <span class="checkmark"></span>
+                                            </label>
+                                            <label class="container">${myQuestion.questionText[i].optionB}
+                                                <input type="radio" name="option_answer_${i+1}" class="option-answer" value="B">
+                                                <span class="checkmark"></span>
+                                            </label>
+                                            <label class="container">${myQuestion.questionText[i].optionC}
+                                                <input type="radio" name="option_answer_${i+1}" class="option-answer" value="C">
+                                                <span class="checkmark"></span>
+                                            </label>
+                                            
+                                        </div>
+                                    </div>`;
+	}
+	console.log("fnish generate question");
+	
+	
+}
+
+
+function generateQuestionAnswerAnother() {
+    let divQuestionAnswer = document.querySelector('#div-question_answer');
+	var myQuestion = getQuestionFromUnit()[0];
+    for(const [i, v] of myQuestion.question.entries()) {
+        divQuestionAnswer.innerHTML = divQuestionAnswer.innerHTML
+                                    + `<div class="choices">
+                                        <div>
+                                        <p id="test">${i+11 + ":"}</p>
+                                        </div>
+                                        <div>
+                                            <label class="container">${myQuestion.questionText[i].optionA}
+                                                <input type="radio" name="option_answer_${i+1}" class="option-answer" value="A">
+                                                <span class="checkmark"></span>
+                                            </label>
+                                            <label class="container">${myQuestion.questionText[i].optionB}
+                                                <input type="radio" name="option_answer_${i+1}" class="option-answer" value="B">
+                                                <span class="checkmark"></span>
+                                            </label>
+                                            <label class="container">${myQuestion.questionText[i].optionC}
+                                                <input type="radio" name="option_answer_${i+1}" class="option-answer" value="C">
+                                                <span class="checkmark"></span>
+                                            </label>
+                                            
+                                        </div>
+                                    </div>`;
+	}
+	console.log("fnish generate question");
+	
+}
+
+
+var level = sessionStorage.getItem('unitIframe');
+
+if(level > 6){
+	generateQuestionAnswerAnother();
+}else{
+	generateQuestionAnswer();
+}
+// set up video image and link
+var video = document.getElementById('myVideoTag');
+var sources = video.getElementsByTagName('source');
+video.setAttribute("poster",getQuestionFromUnit()[0].video.cover);
+sources[0].src = getQuestionFromUnit()[0].video.src;
+video.load();
---
+++ gate/my.js
@@ -0,0 +1,306 @@
+var oldEl = null;
+var oldEl2 = null;
+var oldColor = null;
+var oldColorMobile = null;
+var oldDropBtn = null;
+
+// Use for setting the iframe and the unit
+function setIframe(link,type,unit){
+    $(".iframe-src").attr('src',link);
+    $("#testTypeTemplate").val(type);
+    $("#testUnitTemplate").val(unit);
+    
+    //set up for the back and next
+    if(type == "1vo"){
+        $("#navType").val(0);
+    }else if(type == "2gr"){
+        $("#navType").val(1);
+    }else if(type == "3gr"){
+        $("#navType").val(2);       
+    }else if(type == "4co"){
+        $("#navType").val(3);       
+    }else if(type == "5li"){
+        $("#navType").val(4);        
+    }else if(type == "6to"){
+        $("#navType").val(5);        
+    }
+    // set the unit for back and next
+    $("#navUnit").val(unit-1);
+
+    // style the dropdown background
+    colorNav(unit)
+
+    // Keep the info of the current page incase we refresh
+    sessionStorage.setItem('previousType', parseInt($('#navType').val())+1);
+    sessionStorage.setItem('previousUnit', parseInt($('#navUnit').val())+1);
+
+    sessionStorage.setItem('unitIframe', (parseInt(unit)));
+
+    //set body to make fixed element in 5li and 6to
+    if($('#navType').val() == 5 || $('#navType').val() == 4){
+        $('body').css('overflow','hidden');
+    }else{
+        $('body').css('overflow','scroll');
+    }
+
+}
+
+function goBack() {
+    var type = $("#navType").val();
+    var unit = $("#navUnit").val();
+
+    if(unit != 0 || type != 0){
+        if(type == 0){
+            unit = parseInt(unit)-1;
+            type = 5;
+        }else{
+            type = parseInt(type)-1;
+        }
+
+        $("#navType").val(type);
+        $("#navUnit").val(unit);
+        
+    }else {
+        alert("This is the beginning of the lesson");
+    }
+
+    $("#navType").val(type);
+    $("#navUnit").val(unit);
+
+    //set background color
+    colorNav(parseInt(unit)+1)
+
+    var link = null;
+    if(parseInt(type) == 0){
+        link = "./1vo";
+    }else if(parseInt(type) == 1){
+        link = "./2gr";
+    }else if(parseInt(type) == 2){
+        link = "./3gr";
+    }else if(parseInt(type) == 3){
+        link = "./4co";
+    }else if(parseInt(type) == 4){
+        link = "./5li";    
+    }else if(parseInt(type) == 5){
+        link = "./6to";       
+    }
+
+    // Keep the info of the current page incase we refresh
+    sessionStorage.setItem('previousType', parseInt($('#navType').val())+1);
+    sessionStorage.setItem('previousUnit', parseInt($('#navUnit').val())+1);
+
+    $(".iframe-src").attr('src',link);
+    sessionStorage.setItem('unitIframe', (parseInt(unit)+1));
+
+    //set body to make fixed element in 5li and 6to
+    if($('#navType').val() == 5 || $('#navType').val() == 4){
+        console.log('hidden');
+        $('body').css('overflow','hidden');
+    }else{
+        console.log('scroll');
+        $('body').css('overflow','scroll');
+    }
+
+}
+
+function goForward() {
+    var type = $("#navType").val();
+    var unit = $("#navUnit").val();
+    type = parseInt(type)+1;
+    if(type > 5){
+        unit = parseInt(unit)+1;
+        type = 0;
+    }
+    
+    if(unit > 11){
+        unit = 11;
+        type = 5;
+        alert("end of the lesson");
+    }
+
+    $("#navType").val(type);
+    $("#navUnit").val(unit);
+
+    //set background color
+    colorNav(parseInt(unit)+1)
+
+    //set body to make fixed element in 5li and 6to
+    if($('#navType').val() == 5 || $('#navType').val() == 4){
+        $('body').css('overflow','hidden');
+    }else{
+        $('body').css('overflow','scroll');
+    }
+
+    var link = null;
+    if(parseInt(type) == 0){
+        link = "./1vo";
+    }else if(parseInt(type) == 1){
+        link = "./2gr";
+    }else if(parseInt(type) == 2){
+        link = "./3gr";
+    }else if(parseInt(type) == 3){
+        link = "./4co";
+    }else if(parseInt(type) == 4){
+        link = "./5li";    
+    }else if(parseInt(type) == 5){
+        link = "./6to";       
+    }
+
+    // Keep the info of the current page incase we refresh
+    sessionStorage.setItem('previousType', parseInt($('#navType').val())+1);
+    sessionStorage.setItem('previousUnit', parseInt($('#navUnit').val())+1);
+
+    $(".iframe-src").attr('src',link);
+    sessionStorage.setItem('unitIframe', (parseInt(unit)+1));
+}
+
+function colorNav(unit) {
+    clearHov()
+
+    // For Desktop
+    if(oldColor != null){
+        $(oldColor).css('background-color','inherit');
+    }
+    var myDropDown = $('.navbar').children()[unit-1];
+    var dropBtn = myDropDown.childNodes[1];
+    var colorGet = $(dropBtn).children()[0]
+    var color = $(colorGet).css('background-color');
+    $(dropBtn).css("background-color",color);
+    oldColor = dropBtn;
+
+    // For Mobile
+    if(oldColorMobile != null){
+        $(oldColorMobile).css('background-color','#ffe4c4');
+    }
+
+    var mobileEl = $('.unitMobile').children()[unit-1]
+    $(mobileEl).css('background-color','#00ffff');
+    oldColorMobile = mobileEl
+}
+
+// use to clear the hover 
+function clearHov() {
+    var getDropdown = $('.dropdown').children()
+    for (i = 0; i < 12; i++) {
+        $(getDropdown[i]).css('background-color','');
+    }
+}
+
+function clearActive() {
+    var getDropdown = $('.dropdown').children()
+    for (i = 0; i < 12; i++) {
+        $(getDropdown[i]).removeClass('active');
+    }  
+}
+
+// put the hover to the previous refresh
+function unitHover(unit){
+    var getDropdown = $('.dropdown').children();
+    var dropBtn = getDropdown[unit].childNodes[1];
+    var color = $(dropBtn).css('background-color');
+    $(getDropdown[unit]).css("background-color",color);
+}
+
+$(".typeHeader").click(function() {
+    // manage the unit zone
+    var el = $(this)[0].children[2];
+    if(oldEl != el){
+        $(el).toggle();
+        $(oldEl).css('display','none');
+        oldEl = el;
+    }else{
+        $(el).toggle();
+        oldEl = el;
+    }
+
+    // manage colr
+    var el2 = $(this)[0].children[0];
+    if(oldEl2 != el2){
+        if($(el2).css('color') == 'rgb(0, 0, 0)'){
+            $(el2).css('color','green');
+        }else {
+            $(el2).css('color','black');
+        }
+        $(oldEl2).css('color','black');
+        oldEl2 = el2;
+    }else{
+        if($(el2).css('color') == 'rgb(0, 0, 0)'){
+            $(el2).css('color','green');
+        }else {
+            $(el2).css('color','black');
+        }
+        oldEl2 = el2;
+    }
+
+    
+});
+
+function convertType(type){
+    if(type == 1){
+        return './1vo';
+    }else if(type == 2){
+        return './2gr';
+    }else if(type == 3){
+        return './3gr';
+    }else if(type == 4){
+        return './4co';
+    }else if(type == 5){
+        return './5li';
+    }else if(type == 6){
+        return './6to';
+    }
+}
+
+$(".mobileMenuButton").click(function() {
+    $(".unitMobile").toggle();
+});
+
+$('.unitSelectTop').click(function(){
+    $(".unitMobile").toggle();
+});
+
+$('.dropbtn').click(function(){
+    var content = $(this).children()[0];
+    clearActive()
+
+    if(oldDropBtn != content){
+        $(content).css('display','block');
+        $(oldDropBtn).css('display','none');
+        $(this).toggleClass( "active" )
+    }else{
+        $(content).toggle();
+    }
+
+
+    //set old dropbtn
+    oldDropBtn = content;
+
+});
+
+// set back ground color first
+colorNav(1)
+// get the previous lesson if we refresh
+if(sessionStorage.getItem('previousType') != null){
+    $(".iframe-src").attr('src',convertType(sessionStorage.getItem('previousType')));
+    setTimeout(function(){
+        var iframe = document.getElementById("iframe-src2");
+        iframe.contentWindow.postMessage(sessionStorage.getItem('previousUnit'),window.location.origin);
+    },200)
+
+    //Update the input so the next and previous bar work too
+    $('#navUnit').val(sessionStorage.getItem('previousUnit')-1);
+    $('#navType').val(sessionStorage.getItem('previousType')-1);
+
+    // clear all of the hover
+    clearHov()
+
+    // put in the previous hover
+    unitHover(sessionStorage.getItem('previousUnit')-1)
+
+}
+
+
+
+
+
+
